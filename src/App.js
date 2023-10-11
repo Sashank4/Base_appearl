6f843dab3fc3106e4d4b4d16dc70b969aaf8bc61
sashank <sashanlt4@gmail.com>
2023-10-11 11:29:53 +0530

    mobile design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
-import Img from './assets/images/hero-desktop.jpg'
 import logo from './assets/images/logo.svg'
 import arrow from './assets/images/icon-arrow.svg'
 function App() {
   return (
-    <div className="App">
-      <div className='content'>
-         <img src={logo} alt='logo'/>
-         <h1><span className='regular'>WE'RE</span> COMING SOON</h1>
-         <p className='des'>Hello fellow shopers! We're currently building our new fashion store. Add your Email below to stay 
-         up-to-date with announcements and launching deals.</p>
-         <form>
-           <input type='email' id='email' placeholder='Email Address'/>
-           <button type='submit'><img src={arrow} alt='Enter'/></button>
-         </form>
-         <p className='errorMsg'>Please provide a valid email</p>
-      </div>
-      
-      <div className='image' >
-        <img  src={Img} alt='hero'/>
-        </div>
+    <>
+    <img className='logo' src={logo} alt='logo'/>
+    
+          
+          <div className='container'>
 
-    </div>
+            <div className='left'>
+              <h1><span className='regular'>WE'RE</span> COMING <br/> SOON</h1>
+              <p className='des'>Hello fellow shopers! We're currently building our new fashion store. Add your Email below to stay 
+              up-to-date with announcements and launching deals.</p>
+              <form >
+                <input  type='email' id='email' placeholder='Email Address'/>
+                <button  type='submit'><img src={arrow} alt='Enter'/></button>
+                
+              </form>
+              
+            </div>   
+          
+            <div className='right' ></div>
+
+          </div>        
+          </>
   );
 }
 
